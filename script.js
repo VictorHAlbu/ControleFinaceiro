ccc421c72959230c7caf1fc873400727be021c8e
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-02-19 15:41:57 -0300

    Fazendo remove de transações

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const transactionsUI = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount = document.querySelector('#amount')
+console.log({inputTransactionName, inputTransactionAmount})
+
+
 
 const dummyTransactions = [
 
@@ -8,6 +17,13 @@ const dummyTransactions = [
     {id: 4, name: 'Violão', amount: 150}
 ]
 
+const removeTransaction = ID => {
+    dummyTransactions = dummyTransactions.filter(transaction => transaction.id !== ID)
+    init()
+}
+
+
+
 const addTransactionIntoDOM = transaction => {
     const operator = transaction.amount < 0 ? '-' : '+'
     const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
@@ -16,7 +32,11 @@ const addTransactionIntoDOM = transaction => {
 
     li.classList.add(CSSClass)
     li.innerHTML = `
-        ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+        ${transaction.name}
+         <span>${operator} R$ ${amountWithoutOperator}</span>
+         <button class="delete-btn" onClick="removeTransaction(${transaction.id})">
+         x
+         </button>
     `
    transactionsUI.append(li)
    //existe tabém o metodo preppend que invoca mais ou menos a estrutra pilha
@@ -34,12 +54,47 @@ const updateBalanceValues = () =>{
     .filter(value => value > 0)
     .reduce((acumulator, value) => acumulator + value,0)
     .toFixed(2)
-    console.log(income)
+
+    const expense = Math.abs(transactionsAmounts
+    .filter(value => value < 0)
+    .reduce((acumulator, value) => acumulator + value, 0))
+    .toFixed(2)
+
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
 }
 
 const init = () => {
+    transactionsUI.innerHTML = ''
     dummyTransactions.forEach(addTransactionIntoDOM)
     updateBalanceValues()
 }
 
 init()
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const transactionName = inputTransactionName.value.trim()
+    const transactionAmount = inputTransactionAmount.value.trim()
+
+    if (transactionName === '' || transactionAmount === '' ) {
+        alert('Por favor, preencha tanto nome quanto valor da transação ')
+        return
+    }
+
+    const transaction = {
+        id: generateID(), 
+        name: transactionName, 
+        amount: Number(transactionAmount) 
+    }
+
+    dummyTransactions.push(transaction)
+    init()
+
+    inputTransactionName.value = ''
+    inputTransactionAmount.value = ''
+})

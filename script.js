5c1c148b784971e9b73624f2e76420def781d685
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-02-19 13:59:35 -0300

    calculo total, despesas e receitas

---
+++ script.js
@@ -0,0 +1,45 @@
+const transactionsUI = document.querySelector('#transactions')
+
+const dummyTransactions = [
+
+    {id: 1, name: 'Bolo de Brigadeiro', amount: -20},
+    {id: 2, name: 'Salário', amount: 300},
+    {id: 3, name: 'Torta de frango', amount: -10},
+    {id: 4, name: 'Violão', amount: 150}
+]
+
+const addTransactionIntoDOM = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+'
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(transaction.amount)
+    const li = document.createElement('li')
+
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+        ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+    `
+   transactionsUI.append(li)
+   //existe tabém o metodo preppend que invoca mais ou menos a estrutra pilha
+}
+
+const updateBalanceValues = () =>{
+    const transactionsAmounts = dummyTransactions
+    .map(transaction => transaction.amount)
+    
+    const total = transactionsAmounts
+    .reduce((acumulator, transaction) => acumulator + transaction,0)
+    .toFixed(2)
+    
+    const income = transactionsAmounts
+    .filter(value => value > 0)
+    .reduce((acumulator, value) => acumulator + value,0)
+    .toFixed(2)
+    console.log(income)
+}
+
+const init = () => {
+    dummyTransactions.forEach(addTransactionIntoDOM)
+    updateBalanceValues()
+}
+
+init()
